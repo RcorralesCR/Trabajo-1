7734ed5d7b945e5a96798ceafa3c71e6eefe1096
rjesu <rjesu@DESKTOP-GMUBEQ5>
2023-06-05 17:05:01 -0600

    Trabajo #1-Final

--- Trabajo_1/src/test/java/com/trabajo_1/Trabajo1ApplicationTests.java
+++ Trabajo_1/src/test/java/com/trabajo_1/Trabajo1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Trabajo1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
